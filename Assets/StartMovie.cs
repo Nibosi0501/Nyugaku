283818bfc85dc555d9be2df0739d8b8356edc29c
Nibosi <k22137kk@aitech.ac.jp>
2025-03-30 17:44:14 +0900

    a

--- Assets/StartMovie.cs
+++ Assets/StartMovie.cs
@@ -7,21 +7,33 @@ public class StartMovie : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
 
-    [SerializeField] private GameObject display1Background;
-    [SerializeField] private GameObject display2Background;
+    [SerializeField] private GameObject QRpanel;
+    [SerializeField] private GameObject QRCode1;
+    [SerializeField] private GameObject QRCode2;
+
+    private bool isVideoPrepared = false;
 
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
         videoPlayer.Prepare();
         videoPlayer.prepareCompleted += OnVideoPrepared;
-
     }
 
-    void OnVideoPrepared(VideoPlayer vp)
+    void Update()
     {
-        display1Background.SetActive(false);
-        display2Background.SetActive(false);
-        videoPlayer.Play();
+        if (Input.GetKeyDown(KeyCode.Return) && isVideoPrepared)
+        {
+            QRCode1.SetActive(false);
+            QRCode2.SetActive(false);
+            QRpanel.SetActive(false);
+
+            videoPlayer.Play();
+        }
+    }
+
+    private void OnVideoPrepared(VideoPlayer vp)
+    {
+        isVideoPrepared = true;
     }
 }
--- Assets/Zunda/Zunda.cs
+++ Assets/Zunda/Zunda.cs
@@ -40,7 +40,7 @@ public class Zunda : MonoBehaviour
 
     IEnumerator WaitForNextScene()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(20.0f);
         changeScene.NextScene();
     }
 
